553ffaf457ba3a89c5988a1a82db6ed270dcc47d
dpsiu <denversiu24@gmail.com>
2023-11-08 15:37:17 -0600

    refactor: turned footer into component
    added website images

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,16 +2,19 @@ export function Footer() {
   return (
     <>
       <div className="footer">
-        <p>2023</p>
-        <a href="https://www.linkedin.com/in/denver-siu/">
-          <h4>Denver Siu</h4>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
-        </a>
-        <a href="https://github.com/dpsiu">
-          <h4>dpsiu</h4>
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-        </a>
-      </div>
+        <p>Site built with React</p>
+        <div className="footer-links">
+            <p>2023</p>
+            <a href="https://www.linkedin.com/in/denver-siu/">
+                <p>Denver Siu</p>
+                <span className="social-icon devicon-linkedin-plain"></span>
+            </a>
+            <a href="https://github.com/dpsiu">
+                <p>dpsiu</p>
+                <span className="social-icon devicon-github-original"></span>
+            </a>
+        </div>
+    </div>
     </>
   );
 }
