da3ed1626cf6450e504e5ccb68708c6aa65e750a
dpsiu <denversiu24@gmail.com>
2023-11-10 00:25:45 -0600

    WIP: fix broken image links from netlify deploy

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -16,7 +16,7 @@ export default function NavBar() {
             {/* <p className="LeBalein">LeBalein</p> */}
             <img
               className="lebalein-icon"
-              src="Icons/BlueWhaleIcon.png"
+              src="/Icons/BlueWhaleIcon.png"
               alt="Blue Whale Site Icon"
             />
           </Link>
